28123ab820008605d4f3f15a750603e2d6702e5f
Aniket Pandey <aniketp@FreeBSD.org>
2018-06-25 16:14:07 +0530

    Add EOF check preceding break statements
    Include missing headers

--- password/Enclave/Edger8rSyntax/Arrays.cpp
+++ password/Enclave/Edger8rSyntax/Arrays.cpp
@@ -1,40 +1,11 @@
-/*
- * Copyright (C) 2011-2018 Intel Corporation. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *   * Neither the name of Intel Corporation nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-/* Test Array Attributes */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include "sgx_trts.h"
 #include "../Enclave.h"
 #include "Enclave_t.h"
+
 #define MAXLEN 1024
 
 /*
@@ -94,7 +65,11 @@ void viewpassword(char *choice, char *input)
 
 		/* If the first word was not the account, then we skip to the next line */
 		if (strncmp(buff, choice, sizeof(choice))) {
-			while ((c = (char)fgetc(file)) != '\n');
+			while ((c = (char)fgetc(file)) != '\n') {
+				/* Check if we've reached the end of file */
+				if (fgetc(file) == EOF)
+					break;
+			}
 		}
 		else {
 			i = 0;
@@ -103,6 +78,7 @@ void viewpassword(char *choice, char *input)
 				buff[i++] = c;	
 			}
 			strncpy(input, buff, sizeof(buff));
+			break;
 		}
 	}
 	return;
